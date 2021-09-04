12d82435285ea672baa3b2b634a605045111307b
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-09-04 18:06:11 -0300

    ejercicio_1-Parte_1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,6 +8,7 @@ namespace SRP
         public string Titulo { get ; }
         public string Autor { get ; }
         public string Codigo { get ;  }
+        //los dos proximos atributos deberían separarse de esta clase a una nueva que lleve la responsabilidad de almacenar los libros
         public string SectorBiblioteca { get ; set; }
         public string EstanteBiblioteca { get ; set; }
 
@@ -18,7 +19,9 @@ namespace SRP
             this.Codigo = codigo;
         }
 
-        public void AlmacenarLibro(String sector, String estante)
+        public void AlmacenarLibro(String sector, String estante) 
+        //la clase libro debería tener solo la responsabilidad de conocer los libros y su información
+        // el almacenamiento de lo mismos debería recaer en otra clase con sus metodos y atributos propios.
         {
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
