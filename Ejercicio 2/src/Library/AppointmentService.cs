4a53daef5bc0c19d7ca121d809a38206b8a6af9a
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-09-06 15:37:02 -0300

    modificaciones para testeo y appointments

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Text;
+using System.Collections;
 
 namespace Library
 {
     public class AppointmentService
     {
-        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        private static ArrayList scheduledAppointments = new ArrayList(); 
+        //para controlar que no se repitan ids
+
+        public static string CreateAppointment(Pacient name, string id, DateTime date, string appoinmentPlace, Doctor doctorName)
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
             Boolean isValid = true;
 
-            if (string.IsNullOrEmpty(name))
+            if (name.pacientIsValid == false)
             {
-                stringBuilder.Append("Unable to schedule appointment, Name is required\n");
+                stringBuilder.Append("Unable to schedule appointment, problems with pacients register\n");
+                isValid = false;
+            }
+
+            if (doctorName.doctorIsValid == false)
+            {
+                stringBuilder.Append("Unable to schedule appointment, problems with doctor´s register\n");
                 isValid = false;
             }
 
@@ -22,33 +32,100 @@ namespace Library
                 isValid = false;
             }
 
-            if (string.IsNullOrEmpty(phoneNumber))
-            {
-                stringBuilder.Append("Unable to schedule appointment, Phone number is required\n");
-                isValid = false;
-            }
-
             if (string.IsNullOrEmpty(appoinmentPlace))
             {
                 stringBuilder.Append("Unable to schedule appointment, Appoinment place is required\n");
                 isValid = false;
             }
 
-            
-            if (string.IsNullOrEmpty(doctorName))
+            if (scheduledAppointments.Contains(id))
             {
-                stringBuilder.Append("Unable to schedule appointment, Doctor name is required\n");
+                stringBuilder.Append("Unable to schedule appointment, id already taken\n");
                 isValid = false;
-            }
+            }   
 
             if (isValid)
             {
                 stringBuilder.Append("Appoinment Scheduled");
+                scheduledAppointments.Add(id);
             }
 
             return stringBuilder.ToString();
         }
-        /* Esta clase sufre de un exceso de responsabilidades (una falla de SRP) que limita y dificulta sus tareas y mantenimiento. La clase debería solo tener la responsabilidad de
-        guardar la cita, no también tener que tomar la data de cada paciente y doctor y ademas checkear que estos datos son validos*/
+    }
+
+    public class Pacient
+    {
+        public string Name {get; set;}
+        public string Age {get; set;}
+        public string PhoneNumber {get; set;}
+        public Boolean pacientIsValid {get; set;}
+
+        public Pacient (string name, string age, string phonenumber)
+        {
+            this.pacientIsValid = true;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Unable to register pacient, Name is required\n");
+                this.pacientIsValid = false;
+            }
+            else
+            {
+                this.Name = name;
+            }
+            
+            if (string.IsNullOrEmpty(age))
+            {
+                Console.WriteLine("Unable to register pacient, Age is required\n");
+                this.pacientIsValid = false;            
+            }
+            else
+            {
+                this.Age = age;
+            }
+
+            if (string.IsNullOrEmpty(phonenumber))
+            {
+                Console.WriteLine("Unable to register pacient, Phone Number is required\n");
+                this.pacientIsValid = false;
+            }
+            else
+            {
+                this.PhoneNumber = phonenumber;
+            }
+        }
+    }
+
+    public class Doctor
+    {
+        public string Name {get; set;}
+        public string Specialty {get; set;}
+        public Boolean doctorIsValid {get; set;}
+
+        public Doctor (string name, string specialty)
+        {
+            this.doctorIsValid = true;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Unable to register Doctor, Name is required\n");
+                this.doctorIsValid = false;
+            }
+            else
+            {
+                this.Name = name;
+            }
+            
+            if (string.IsNullOrEmpty(specialty))
+            {
+                Console.WriteLine("Unable to register doctor, Specialty is required\n");
+                this.doctorIsValid = false;
+            }
+            else
+            {
+                this.Specialty = specialty;
+            }
+        }
     }
 }
--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,15 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            Pacient paciente1 = new Pacient("Messi", "34", "18001337");
+            Doctor doctor1 = new Doctor("House", "Diagnósticos Médicos");
+            Pacient paciente2 = new Pacient("", "34", "18001337");
+            Doctor doctor2 = new Doctor("", "Diagnósticos Médicos");
+            
+            string appointmentResult = AppointmentService.CreateAppointment(paciente1, "5555-555-555", DateTime.Now, "Wall Street", doctor1);
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            string appointmentResult2 = AppointmentService.CreateAppointment(paciente2, "5555-555-555", DateTime.Now, "Queen Street", doctor2);
             Console.WriteLine(appointmentResult2);
         }
     }
